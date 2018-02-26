c8d75b369a735e97b1ada7184211b236816e5b1a
Yuki Nakatsuru <yuki.nakatsuru@mizuho-ir.co.jp>
2018-02-26 16:40:04 +0900

    delConfirm.go追加

---
+++ chaincode/delConfirm/delConfirm.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+        "bytes"
+        "encoding/json"
+        "fmt"
+        "strconv"
+
+        "github.com/hyperledger/fabric/core/chaincode/shim"
+        sc "github.com/hyperledger/fabric/protos/peer"
+)
+
+// SmartContract structure
+type SmartContract struct {
+}
+
+// MaxNumber structure
+type MaxNumber struct {
+        MaxApplicationNo string `json:"maxApplicationNo"`
+}
+
+// delConfirm Chaincode implementation
+type delConfirm struct {
+        ApplicationNo     string `json:"applicationNo"`
+        SystemNo          string `json:"systemNo"`
+        ProjectName       string `json:"projectName"`
+        DataQuantity      string `json:"dataQuantity"`
+        StartDate         string `json:"startDate"`
+        EndDate           string `json:"endDate"`
+        ApplicationStatus string `json:"applicationStatus"`
+        Registrant        string `json:"registrant"`
+        Rechecker         string `json:"rechecker"`
+        Reviewer          string `json:"reviewer"`
+        Authorizer        string `json:"authorizer"`
+        Detail1           string `json:"detail1"`
+        div1              string `json:"div1"`
+        delDate1          string `json:"delDate1"`
+        Detail2           string `json:"detail2"`
+        div2              string `json:"div2"`
+        delDate2          string `json:"delDate2"`
+        Detail3           string `json:"detail3"`
+        div3              string `json:"div3"`
+        delDate3          string `json:"delDate3"`
+        Detail4           string `json:"detail4"`
+        div4              string `json:"div4"`
+        delDate4          string `json:"delDate4"`
+}
+
+/*
+//Numbering is a function to decide applicationNo
+func (s *SmartContract) Numbering(APIstub shim.ChaincodeStubInterface) sc.Response {
+
+        //      return shim.Success(maxNumber)
+}
+*/
+
+//Init method is called as a result of deployment "delConfirm"
+func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
+
+        var maxNumber = MaxNumber{
+                MaxApplicationNo: "0",
+        }
+        maxNumberAsBytes, _ := json.Marshal(maxNumber)
+        APIstub.PutState("maxApplicationNo", maxNumberAsBytes)
+
+        return shim.Success(nil)
+}
+
+//Invoke method is called as a result of an application request to run the Smart Contract "delConfirm"
+func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
+
+        // Retrieve the requested Smart Contract function and arguments
+        function, args := APIstub.GetFunctionAndParameters()
+        // Route to the appropriate handler function to interact with the ledger
+        if function == "createApplication" {
+                return s.createApplication(APIstub, args)
+        } else if function == "queryApplication" {
+                return s.queryApplication(APIstub, args)
+        } else if function == "queryAllApplications" {
+                return s.queryAllApplications(APIstub)
+        }
+        return shim.Error("Invalid Smart Contract function name.")
+}
+
+func (s *SmartContract) createApplication(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+
+        if len(args) != 22 {
+                return shim.Error("Incorrect number of arguments. Expecting 22")
+        }
+
+        //get maxNumber of application
+        maxNumberAsBytes, _ := APIstub.GetState("maxApplicationNo")
+
+        maxNumber := MaxNumber{}
+        json.Unmarshal(maxNumberAsBytes, &maxNumber)
+
+        var tmpNumber int
+        tmpNumber, _ = strconv.Atoi(maxNumber.MaxApplicationNo)
+        tmpNumber = tmpNumber + 1
+
+        maxNumber.MaxApplicationNo = strconv.Itoa(tmpNumber)
+
+        var delConfirm = delConfirm{
+                ApplicationNo:     maxNumber.MaxApplicationNo,
+                SystemNo:          args[0],
+                ProjectName:       args[1],
+                DataQuantity:      args[2],
+                StartDate:         args[3],
+                EndDate:           args[4],
+                Registrant:        args[5],
+                Rechecker:         args[6],
+                Reviewer:          args[7],
+                Authorizer:        args[8],
+                ApplicationStatus: args[9],
+				Detail1:           args[10],
+				div1:              args[11],
+                delDate1:          args[12],
+				Detail2:           args[13],
+				div2:              args[14],
+                delDate2:          args[15],
+				Detail3:           args[16],
+				div3:              args[17],
+                delDate3:          args[18],
+				Detail4:           args[19],
+				div4:              args[20],
+				delDate4:          args[21],
+        }
+
+        maxNumberAsBytes, _ = json.Marshal(maxNumber)
+        delConfirmAsBytes, _ := json.Marshal(delConfirm)
+
+        APIstub.PutState("maxApplicationNo", maxNumberAsBytes)
+        APIstub.PutState(maxNumber.MaxApplicationNo, delConfirmAsBytes)
+
+        return shim.Success(nil)
+}
+
+
+func (s *SmartContract) queryApplication(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+
+        if len(args) != 1 {
+                return shim.Error("Incorrect number of arguments. Expecting 1")
+        }
+
+        delConfirmAsBytes, _ := APIstub.GetState(args[0])
+        return shim.Success(delConfirmAsBytes)
+}
+
+func (s *SmartContract) queryAllApplications(APIstub shim.ChaincodeStubInterface) sc.Response {
+
+        startKey := "0"
+        endKey := "999"
+
+        resultsIterator, err := APIstub.GetStateByRange(startKey, endKey)
+        if err != nil {
+                return shim.Error(err.Error())
+        }
+        defer resultsIterator.Close()
+
+        // buffer is a JSON array containing QueryResults
+        var buffer bytes.Buffer
+        buffer.WriteString("[")
+
+        bArrayMemberAlreadyWritten := false
+        for resultsIterator.HasNext() {
+                queryResponse, err := resultsIterator.Next()
+                if err != nil {
+                        return shim.Error(err.Error())
+                }
+                // Add a comma before array members, suppress it for the first array member
+                if bArrayMemberAlreadyWritten == true {
+                        buffer.WriteString(",")
+                }
+                buffer.WriteString("{\"Key\":")
+                buffer.WriteString("\"")
+                buffer.WriteString(queryResponse.Key)
+                buffer.WriteString("\"")
+
+                buffer.WriteString(", \"Record\":")
+                // Record is a JSON object, so we write as-is
+                buffer.WriteString(string(queryResponse.Value))
+                buffer.WriteString("}")
+                bArrayMemberAlreadyWritten = true
+        }
+        buffer.WriteString("]")
+
+        fmt.Printf("- queryAllApplications:\n%s\n", buffer.String())
+
+        return shim.Success(buffer.Bytes())
+}
+
+// The main function is only relevant in unit test mode. Only included here for completeness.
+func main() {
+        // Create a new Smart Contract
+        err := shim.Start(new(SmartContract))
+        if err != nil {
+                fmt.Printf("Error creating new Smart Contract: %s", err)
+        }
+}
